69230dfcb26abe253e5befedc68b3d0157fcf3b8
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-09 11:22:50 +0300

    Tehtävä 2.9 Puhelinluettelo step 4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -9,11 +9,17 @@ const Person = ({ key, name, number }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
-  const addInfo = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
     if (persons.filter((person) => person.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`);
@@ -38,10 +44,24 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const personsToShow =
+    newFilter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(newFilter.toLowerCase())
+        );
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addInfo}>
+      Filter shown with{" "}
+      <input value={newFilter} onChange={handleFilterChange} />
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -49,14 +69,15 @@ const App = () => {
         <div>
           number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
-        {/*<div>debug: {newName}</div>*/}
+        {/*<div>debug: {newNumber}</div>*/}
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <p>{newFilter.length === 0 ? <></> : <span>Filter in use</span>}</p>
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <Person key={person.name} name={person.name} number={person.number} />
         ))}
       </ul>
