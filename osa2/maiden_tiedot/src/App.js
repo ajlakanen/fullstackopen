1ec8a63f72c2b09f79ef02321845fb28c91a0192
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-10 01:02:05 +0300

    Tehtävä 2.14 Maiden tiedot step 3

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -32,6 +32,7 @@ const Country = ({ country, toShow, showHandler, isRemovable }) => {
   for (const key in country.languages) {
     languages.push(country.languages[key]);
   }
+
   return (
     <div>
       <h1>
@@ -51,7 +52,46 @@ const Country = ({ country, toShow, showHandler, isRemovable }) => {
         ))}
       </ul>
       <img src={country.flags.svg} width="200px" alt="asdf" />
+      <Weather country={country} />
     </div>
+  );
+};
+
+const Weather = ({ country }) => {
+  const [temp, setTemp] = useState(0);
+  const [icon, setIcon] = useState("");
+  const [wind, setWind] = useState(0);
+
+  const lat = country.latlng[0];
+  const lon = country.latlng[1];
+  const api_key = process.env.REACT_APP_WEATHER_API_KEY;
+  console.log(lat, lon, api_key);
+  console.log(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
+  );
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setTemp(response.data.main.temp);
+        setIcon(
+          `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+        setWind(response.data.wind.speed);
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>Weather in {country.capital[0]}</h2>
+      <p>Temperature: {(temp - 273.15).toPrecision(3)} &deg;C</p>
+      <img src={icon} />
+      <p>Wind: {wind} m/s</p>
+    </>
   );
 };
 
